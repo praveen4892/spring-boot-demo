cfe5eea1af51b23d607f8a169b68cb7eae9b1400
Sunny Vats <layers_tipsier_0n@icloud.com>
2024-06-15 17:53:23 +0530

    Init dev branch for Week-5(24th-week) - Entities and ORM
     - Add Product entity.
     - Add Lombok annotation example.
     - Remove Unused service.
     - Refactor Home Controller to contain only web APIs.
     - Add H2 in-memory database configurations to application.properties.
     - Add spring-data-jpa and h2 dependencies to our project.

--- src/main/java/com/dev/sunny/springbootdemo/controllers/HomeController.java
+++ src/main/java/com/dev/sunny/springbootdemo/controllers/HomeController.java
@@ -1,35 +1,21 @@
 package com.dev.sunny.springbootdemo.controllers;
 
-import com.dev.sunny.springbootdemo.services.GreetingService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Map;
 
-// What is Spring IoC Container and Dependency Injection?
-// What is dependency?
 @Controller
+@Slf4j
 public class HomeController {
-
-    private final GreetingService greetingService;
-
-    public HomeController(GreetingService greetingService) {
-        this.greetingService = greetingService;
-    }
 
     @GetMapping(value = "/hello")
     public String hello(Model model, @RequestParam(name = "greeterName") String name) {
         model.addAttribute("name", name);
+        log.info("Model: {}", model);
         return "index";
-    }
-
-    @GetMapping(value = "/json", produces = "application/json")
-    @ResponseBody
-    public Map<String, String> helloJson(@RequestParam(name = "greeterName") String name) {
-        return greetingService.greet(name);
     }
 
 }
